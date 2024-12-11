6cf448f2df78f48f5627a3eb998005f83c55090f
Pius Aaron <piuschbz@gmail.com>
2024-12-11 06:30:24 +0100

    Add: basic crud ops on db level

--- app/utils/database.py
+++ app/utils/database.py
@@ -4,9 +4,10 @@ database.py
 storage connection set up
 '''
 
+from ..models import User, Task, TaskList
+from ..models.base_model import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from .models import User, TaskFolder, Task, Base
 
 
 class DB():
@@ -20,10 +21,46 @@ class DB():
     @property
     def engine(self):
         return self.__engine
+
+
     @property
     def session(self):
         return self.__session
+
     
+    def new(self, obj):
+        '''Stages a new object to db'''
+
+        if obj.__class__ not in (Task, User, TaskList):
+            raise Exception('DBError: Unsupported object type')
+        self.session.add(obj)
+
+
+    def find_object_by_id(self, cls, _id):
+        
+        if cls not in (Task, User, TaskList):
+            raise Exception('DBError: Unsupported object type')
+        return self.session.query(cls).filter_by(id=_id).first()
+
+
+    def get_all(self, cls):
+        
+        if cls not in (Task, User, TaskList):
+            raise Exception('DBError: Unsupported object type')
+
+        session.query(cls).all()
+
+
+    def save(self):
+        ''' commits changes to the db'''
+
+        self.session.commit()
+
+
+    def delete(self, obj):
+        self.session.remove(obj)
+        self.session.commit()
+
 
     def reload(self):
         """
@@ -33,3 +70,7 @@ class DB():
         Base.metadata.create_all(self.__engine)
         session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session)
+
+
+db = DB()
+db.reload()
